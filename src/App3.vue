1919b318ac41e88eda305fddad3b6c1b4dd83447
Rodrigo Monteiro <a100706@alunos.uminho.pt>
2024-06-12 15:55:33 +0100

    changed dist

---
+++ src/App3.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+      <h1>{{ greeting }}</h1>
+      <p>This is a simple Vue.js app!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        greeting: 'Hello, Vue.js!'
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  #app {
+    font-family: Arial, sans-serif;
+    text-align: center;
+  }
+  </style>
+  
